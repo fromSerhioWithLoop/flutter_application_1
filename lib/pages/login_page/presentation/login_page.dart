1ae783a141a2e155499303b6485bd41dbb13d90c
Serhio <gatfor@mail.ru>
2022-08-29 23:06:19 +0300

    pox

--- lib/pages/login_page/presentation/login_page.dart
+++ lib/pages/login_page/presentation/login_page.dart
@@ -9,46 +9,6 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageA();
 }
 
-class _LoginPage extends State<LoginPage> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Enter your email',
-            ),
-            validator: (String? value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Validate will return true if the form is valid, or false if
-                // the form is invalid.
-                if (_formKey.currentState!.validate()) {
-                  // Process data.
-                }
-              },
-              child: const Text('Submit'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class _LoginPageA extends State<LoginPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final LoginData _loginData = LoginData();
@@ -107,16 +67,15 @@ class _LoginPageA extends State<LoginPage> {
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
-                      child: SizedBox(
-                        height: 50, width: 100,
-                        child: ExampleButton(
-                          child: const Text('login'),
-                          buttonAction: () {
-                          
-                        },),
-                      )
-                    ),
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: SizedBox(
+                          height: 50,
+                          width: 100,
+                          child: ExampleButton(
+                            child: const Text('login'),
+                            buttonAction: () {},
+                          ),
+                        )),
                   ],
                 ),
               ],
